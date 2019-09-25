47768fe7d78e1133d26001c09e6d29656edaf8be
a3046 <630116001@qq.com>
2019-09-26 01:39:03 +0800

    lx

--- src/components/main/Tencent.js
+++ src/components/main/Tencent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactQMap from 'react-qmap';
 import {Link,withRouter} from "react-router-dom";
 import $ from  'jquery'
+import './index.less'
 const getContianer = dom => {
     console.log(dom.childNodes)
     
@@ -9,16 +10,12 @@ const getContianer = dom => {
     // csssprite.innerHTML = `<Link to="/login"><img  id="lx"   src="${require('../../style/imgs/spot_location.png')}" style="width: 22px;height: 100%"/><p style="color:red">华北油田采油三厂楚一联合注水站</p></Link>`;
     middleControl.style.cssText =
         'width: 123px;height: 30px;position: absolute;left: 50%;top: 50%;z-index: 999;margin-left: -23px;margin-top: -23px;';
-    middleControl.innerHTML = `<Link to="/login"><img  id="lx"   src="${require('../../style/imgs/spot_location.png')}" style="width: 22px;height: 100%"/><p style="color:red">华北油田采油三厂楚一联合注水站</p></Link>`;
+    middleControl.innerHTML = `<Link to="/login></Link>`;
     dom.appendChild(middleControl);
     // let csssprite=dom.childNodes('.csssprite')
         // console.log(csssprite)
-    let lx=document.getElementById('lx')
 
-    lx.onclick=function(){
 
-        window.document.location.href="#/app/waterData/allData"
-    }
 };
 let height = document.body.clientHeight-90;
 let classMap, windowMap;
@@ -41,17 +38,15 @@ class Tencent extends React.Component {
 
     }
     init(wMap){
-        var center = wMap.LatLng(39.916527,116.397128);
-
-    
+        var center = wMap.LatLng(32.1756124785,92.0214843750);
         var anchor =new wMap.Point(6, 6),
         scaleSize=new wMap.Size(22,30),
         origin =new wMap.Point(0, 0),
-        size =new wMap.Size(24, 30);
+        size =new wMap.Size(300, 50);
         var icon =new wMap.MarkerImage(require('../../style/imgs/spot_location.png'),size, origin, anchor,scaleSize);
         //文字描述
-        var content = "<span style='color:red;width:100px;'>华北油田采油三厂楚一联合注水站</span>";
-        var decoration = new wMap.MarkerDecoration(content, new qq.maps.Point(0, 30));
+        var content = "<div class='mapct'><span style='color:red;width:100px;'>华北油田采油三厂楚一联合注水站</span></div>";
+        var decoration = new wMap.MarkerDecoration(content, new wMap.Point(0, 30));
         var marker = new wMap.Marker({
             map: classMap,
             icon: icon,
@@ -60,6 +55,10 @@ class Tencent extends React.Component {
             decoration: decoration
                 }
             );
+            wMap.event.addListener(marker, 'click', function() {
+                        window.document.location.href="#/app/waterData/allData"
+           
+            });
     }
     _getMap = (map, wMap) => {
         classMap = map;
