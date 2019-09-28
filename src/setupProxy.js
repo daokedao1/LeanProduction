5ef710efcf6b072bf74a35deb3feb1f5306a0f69
zhuzy01 <quhao@missfresh.com>
2019-09-28 19:56:40 +0800

    22

---
+++ src/setupProxy.js
@@ -0,0 +1,20 @@
+module.exports = function(app) {
+  // ...You can now register proxies as you wish!
+  app.use(proxy('/login', {
+    target: "http://119.90.248.34:51029/",
+    secure: false,
+    changeOrigin: true,
+    pathRewrite: {
+     "/login": "/login"
+    },
+   }));
+   app.use(proxy('/wTimeData', {
+    target: 'http://172.19.5.34:9531',
+    secure: false,
+    changeOrigin: true,
+    pathRewrite: {
+     "/wTimeData": "/wTimeData"
+    },
+   }));
+  //app.use(proxy('/apc', { target: 'http://172.19.5.34:9531' }));
+};
