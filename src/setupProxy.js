4eb8db36ad815327cf4a5175494c9913c6f0db45
zhuzy01 <quhao@missfresh.com>
2019-09-28 20:09:27 +0800

    嗯嗯

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,21 +1,7 @@
 const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
   // ...You can now register proxies as you wish!
-  app.use(proxy('/login', {
-    target: "http://119.90.248.34:51029/",
-    secure: false,
-    changeOrigin: true,
-    pathRewrite: {
-     "/login": "/login"
-    },
-   }));
-   app.use(proxy('/wTimeData', {
-    target: 'http://172.19.5.34:9531',
-    secure: false,
-    changeOrigin: true,
-    pathRewrite: {
-     "/wTimeData": "/wTimeData"
-    },
-   }));
-  //app.use(proxy('/apc', { target: 'http://172.19.5.34:9531' }));
+
+   app.use(proxy('/login', { target: "http://119.90.248.34:51029/"}));
+  app.use(proxy('/wTimeData', { target: "http://119.90.248.34:51029/"}));
 };
