5d37f8c057b8e3f67155c5875eaceaae0a90f8da
zhuzy01 <quhao@missfresh.com>
2019-09-28 20:13:09 +0800

    no message

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,6 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
   // ...You can now register proxies as you wish!
 
-   app.use(proxy('/login', { target: "http://119.90.248.34:51029/"}));
+  app.use(proxy('/login', { target: "http://119.90.248.34:51029/"}));
   app.use(proxy('/wTimeData', { target: "http://119.90.248.34:51029/"}));
 };
