f1f51e1df0f9174bb460fe4e0f94114573706a3d
zhuzy01 <quhao@missfresh.com>
2019-09-28 16:36:42 +0800

    rr

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,24 +2,32 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
   // ...You can now register proxies as you wish!
-  // app.use(proxy('/api', {
-  //   target: 'http://172.19.5.35:9536',
-  //   secure: false,
-  //   changeOrigin: true,
-  //   pathRewrite: {
-  //    "^/api": "/"
-  //   },
-  //  }));
-  //  app.use(proxy('/apc', {
-  //   target: 'http://172.19.5.34:9531',
-  //   secure: false,
-  //   changeOrigin: true,
-  //   pathRewrite: {
-  //    "^/apc": "/"
-  //   },
-  //  }));
+  app.use(proxy('/wTimeData', {
+    target: "http://119.90.248.34:51029/",
+    secure: false,
+    changeOrigin: true,
+    pathRewrite: {
+     "^/wTimeData": "/wTimeData"
+    },
+   }));
+   app.use(proxy('/wInfo', {
+    target: "http://119.90.248.34:51029/",
+    secure: false,
+    changeOrigin: true,
+    pathRewrite: {
+     "^/wInfo": "/wInfo"
+    },
+   }));
+   app.use(proxy('/api', {
+    target: "http://39.98.215.185:8088/",
+    secure: false,
+    changeOrigin: true,
+    pathRewrite: {
+     "^/api": "/api"
+    },
+   }));
 
-  app.use(proxy('/wTimeData', { target: "http://119.90.248.34:51029/" }));
-  app.use(proxy('/wInfo', { target: "http://119.90.248.34:51029/" }));
-  app.use(proxy('/api', { target: "http://39.98.215.185:8088/" }));
+  // app.use(proxy('/wTimeData', { target: "http://119.90.248.34:51029/" }));
+  // app.use(proxy('/wInfo', { target: "http://119.90.248.34:51029/" }));
+  // app.use(proxy('/api', { target: "http://39.98.215.185:8088/" }));
 };
