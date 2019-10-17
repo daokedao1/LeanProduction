cf4a81928398457641b3d63509b78dfb810fc978
a3046 <630116001@qq.com>
2019-10-17 21:09:36 +0800

    lx

---
+++ src/components/waterData/countNum.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Button,Row, Col, Car, Card} from 'antd';
+
+class Demo extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      count:0
+    }
+   let timerCount = setInterval(() => {
+      this.setState((preState) =>({
+        count: preState.count+1,
+      }),() => {
+        
+        if(this.state.count === 10){
+          this.setState({count:0})
+          // clearInterval(this.timerCount);
+        }
+      });
+    }, 1000)
+  }
+  render() {
+    return (
+          <Button type="primary">{`${this.state.count}秒后自动刷新`}</Button>
+    )
+  }
+}
+
+export default Demo
